5abc0209c50c376f91bf467f2daa55ba5f408942
Andrew Duncan <duncanaj@student.unimelb.edu.au>
2025-05-09 20:58:38 +1000

    Update v3_preproc.sh

--- v3_preproc.sh
+++ v3_preproc.sh
@@ -34,6 +34,7 @@ for R1 in "$RAW_DIR"/*_R1_001.fastq.gz; do
   -2 "$R2" \
   -t "$THREADS" \
   "$PHIX_R1" "$PHIX_R2"
+  -F
   cd - || exit 1
   if [[ $? -ne 0 ]]; then
     echo "ERROR: hts_SeqScreener failed on $SAMPLE" >&2
