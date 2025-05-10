c290a30e967479e8d8b76259612e9e3c39c37de2
Andrew Duncan <duncanaj@student.unimelb.edu.au>
2025-05-10 15:29:34 +1000

    Update v3_preproc.sh

--- v3_preproc.sh
+++ v3_preproc.sh
@@ -29,12 +29,11 @@ for R1 in "$RAW_DIR"/*_R1_001.fastq.gz; do
   # ========== PHIX REMOVAL ==========
   echo "Running hts_SeqScreener on $SAMPLE"
   cd "$intermediate_dir" || exit 1
-  hts_SeqScreener phix \
+ hts_SeqScreener phix \
   -1 "$R1" \
   -2 "$R2" \
   -t "$THREADS" \
-  "$PHIX_R1" "$PHIX_R2"
-  -F
+  "$PHIX_R1" "$PHIX_R2" -F
   cd - || exit 1
   if [[ $? -ne 0 ]]; then
     echo "ERROR: hts_SeqScreener failed on $SAMPLE" >&2
